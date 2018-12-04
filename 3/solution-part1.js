5c50522f1c7aca4ac039acbcd9cf9a189eb5b047
WillQi <williamqipizza@gmail.com>
2018-12-03 21:43:29 -0700

    better part 1

--- 3/solution-part1.js
+++ 3/solution-part1.js
@@ -1,35 +1,38 @@
 const {readFileSync} = require("fs");
 const {join} = require("path");
 
-let input = readFileSync(join(__dirname, "input.txt"), "utf8").replace(/\r/g, "").split("\n");
+const input = readFileSync(join(__dirname, "input.txt"), "utf8").replace(/\r/g, "").split("\n");
 
-// input = [
-//     "#1 @ 1,3: 4x4",
-//     "#2 @ 3,1: 4x4",
-//     "#3 @ 5,5: 2x2"
-// ];
-
-const quilt = Array.from({length: 10000}).map(r => Array.from({length: 1000}).map(() => 0));
-
-//const quilt = Array.from({length: 9}).map(r => Array.from({length: 11}).map(() => 0))
+const fabric = Array.from({length: 1000}).map(() => Array.from({length: 1000}));
 
 let overwrote = 0;
 
-const fillBlock = (x, y, id) => {
-    if (quilt[y][x] === Infinity) return;
-    if (quilt[y][x] !== 0) quilt[y][x] = Infinity, overwrote++;
-    else quilt[y][x] = id;
+const fillBlock = (x, y) => {
+    switch (fabric[y][x]) {
+        case 0:
+            fabric[y][x] = 1;
+            overwrote++;
+        break;
+        case 1:
+        break;
+        default:
+            fabric[y][x] = 0;
+        break;
+    }
 };
 
-
 for (const val of input) {
-    const [id, symbol, coordinates, size] = val.split(" ");
-    const [x, y] = coordinates.slice(0, -1).split(",").map(Number);
-    const [width, height] = size.split("x").map(Number);
-    for (let qY = y; qY < y + height; qY++) {
-        for (let qX = x; qX < x + width; qX++) {
-            fillBlock(qX, qY, Number(id.slice(1)));
-        }
+    const values = val.split(/ /);
+    let [, , coords, size] = values;
+    const splitCoords = coords.slice(0, -1).split(",");
+    const [x, y] = splitCoords.map(Number);
+    const splitSize = size.split("x");
+    const [width, height] = splitSize.map(Number);
+    const endY = y + height;
+    const endX = x + width;
+    for (let qY = y; qY < endY; qY++) {
+        for (let qX = x; qX < endX; qX++) fillBlock(qX, qY);
     }
 }
+
 console.log(overwrote);
